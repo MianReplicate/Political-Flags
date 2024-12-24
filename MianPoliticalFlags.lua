7fa562ef871a43c2c0ca48dc18726e1132c50d7f
Mian <docgoosenn@gmail.com>
2024-12-24 01:46:04 -0800

    - Added versioning scheme - Editing for compatibility with the framework

--- MianPoliticalFlags.lua
+++ MianPoliticalFlags.lua
@@ -4,9 +4,12 @@ behaviour(mutatorName)
 function MianPoliticalFlags:Awake()
 	self.frameworkName = "Custom Flag Framework"
 	self.dataContainer = self.gameObject.GetComponent(DataContainer)
-	self.CustomFlags = self.dataContainer.GetTextureArray("Flag")
-	self.CustomFlagTeamColors = self.dataContainer.GetColorArray("Flag")
+	self.version = "1.0.0"
+	self.frameworkVersion = "1.0.0"
+	self.customFlags = self.dataContainer.GetTextureArray("Flag")
+	self.customFlagToTeamColors = self.dataContainer.GetColorArray("Flag")
 	self.cover = self.dataContainer.GetTexture("Cover")
+	self.mutatorName = mutatorName
 end
 
 function MianPoliticalFlags:Start()
@@ -17,5 +20,5 @@ function MianPoliticalFlags:Start()
 		error("Framework was not found! Please make sure the Custom Flags Framework mutator has been enabled.")
 	end
 
-	self.framework:addTexturePack(mutatorName, self)
+	self.framework:addTexturePack(self)
 end
