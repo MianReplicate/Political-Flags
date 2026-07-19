5ccb2f010ec41ac9db9c68c70e5d2cc8938853cf
MianReplicate <docgoosenn@gmail.com>
2026-07-18 21:36:10 -0700

    Updated pack with new framework standards

--- MianPoliticalFlags.lua
+++ MianPoliticalFlags.lua
@@ -2,21 +2,9 @@ local mutatorName = "MianPoliticalFlags"
 behaviour(mutatorName)
 
 function MianPoliticalFlags:Awake()
-	self.frameworkName = "Custom Flag Framework"
 	self.dataContainer = self.gameObject.GetComponent(DataContainer)
 	self.CustomFlags = self.dataContainer.GetTextureArray("Flag")
 	self.CustomFlagToTeamColors = self.dataContainer.GetColorArray("Flag")
 	self.cover = self.dataContainer.GetTexture("Cover")
 	self.name = mutatorName
-end
-
-function MianPoliticalFlags:Start()
-	local obj = GameObject.Find(self.frameworkName)
-	if(obj) then
-		self.framework = ScriptedBehaviour.GetScript(obj)
-	else
-		error("Framework was not found! Please make sure the Custom Flags Framework mutator has been enabled.")
-	end
-
-	self.framework:addFlagPack(self)
 end
